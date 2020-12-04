de4d453907671362379c5593d684f358bba1410a
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-12-04 21:54:56 +0300

    control whether secret key is greater than 26 and if so, will take the remainder and reassign to  the key value

--- W2CaesarCipher/cipher.c
+++ W2CaesarCipher/cipher.c
@@ -9,9 +9,8 @@ int main( int argc, char *argv[] )
 
     printf("The Application %s is waiting for your command \n", argv[0]);
     sleep(1);
-    
     int key;
-
+    // Argument Filtering. Now It accepts only positive integers
     if(argc<2)
     {
       printf("Too few positional arguments\n");
@@ -29,14 +28,21 @@ int main( int argc, char *argv[] )
         if (!isdigit(atoi(argv[1])))
         {
             key = atoi(argv[1]);
-
-            if(key>0){
+            // Controlling whether the key is greater than or equal to 26. It will return the remainder as real key value
+            // the best condition, the value is between 1 and 26
+            if(key>0 && key <=26){
                 printf("key is %d\n",key);
                 key = atoi(argv[1]);
 
             }
+            // assigned key is greater than 26, thus, rotating it and reassigining to key value so there is no overflow in the ascii table.
+            else if(key > 26)
+            {
+                key = key %26;
+                key = atoi(argv[1]);
+            }
             else{
-                printf("Key cannot be a negative integer. Given parameter is %d\n",key);
+                printf("Key cannot be a negative integer or 0. Given parameter is %d\n",key);
                 return EXIT_FAILURE;
             }
             
