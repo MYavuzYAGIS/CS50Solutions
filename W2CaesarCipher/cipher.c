0f6a1662b52c3e6b8bddec9a7e2d40ff658b57aa
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-12-04 20:54:39 +0300

    added a more robust argv check

--- W2CaesarCipher/cipher.c
+++ W2CaesarCipher/cipher.c
@@ -2,29 +2,52 @@
 #include<string.h>
 #include<stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main( int argc, char *argv[] )
 {
 
     printf("The Application %s is waiting for your command \n", argv[0]);
     sleep(1);
-    printf("Please select a functionality and proceed");
+    
+    int key;
 
-
-    if( argc == 2 ) {
-      printf("You selected to %s\n", argv[1]);
-   }
-   else if( argc > 2 ) {
-      printf("Too many positional arguments\n");
-      printf("cipher enc || cipher decr \n");
-   }
-   else {
+    if(argc<2)
+    {
       printf("Too few positional arguments\n");
-      printf("cipher enc || cipher decr \n");
-   }
+      printf("Usage: ./caesar key \n");
+      return EXIT_FAILURE;
+    }
+    else if( argc > 2 ) 
+    {
+      printf("Too many positional arguments\n");
+      printf("Usage: ./caesar key \n");
+      return EXIT_FAILURE;
+    }
+    else if (argc ==2)
+    {
+        if (!isdigit(atoi(argv[1])))
+        {
+            key = atoi(argv[1]);
 
+            if(key>0){
+                printf("key is %d\n",key);
+                key = atoi(argv[1]);
 
+            }
+            else{
+                printf("Key cannot be a negative integer. Given parameter is %d\n",key);
+                return EXIT_FAILURE;
+            }
+            
+        }else{
+            printf("Given argument is not a positive integer\n");
+            EXIT_FAILURE;
+        }
+    }
+ 
 
-
+    
 
 }
+
