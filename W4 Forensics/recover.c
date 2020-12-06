bfb9238cd18aac6d4aa7d552f36ac09aa22a26da
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-12-06 20:00:27 +0300

    locate the forensic image , add few comments and build the boilerplate for the project initialization

---
+++ W4 Forensics/recover.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+
+// the program should only take one argument, which is the name of the forensic image!
+// faulty usage must return 1 and urge the user
+// if forensic image cannot be read, the user must be prompted so.
+// names of the rescued files should be consecutive triple digit integer names - 000.jpg, 001.jpg etc, 
+// is use malloc, no memory leak should happen!
+
+int main(int argc, char *argv[])
+{
+ 
+}
