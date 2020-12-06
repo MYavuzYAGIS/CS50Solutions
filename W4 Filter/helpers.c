d807d96852595008b757ca97f60e0f81fb1c41e8
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-12-06 16:26:20 +0300

    add functionality to grayscale filtering. ./filter -g original.bmp out.bmp

--- W4 Filter/helpers.c
+++ W4 Filter/helpers.c
@@ -3,15 +3,55 @@
 
 // image is a 2D array where Dimensions are height and width. the image is height rows long and witdh columns wide. Width is array within height.
 
-// 
+// We have to make sure that the RGB does not overflow!
+
+
+int NoFlow(int RGB){
+    if(RGB> 255){
+        RGB=255;
+    }
+    return RGB;
+}
+
+
+
 
 
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    
+    float average;
+
+    for(int i = 0; i < width; i++ ){
+        for(int j =0; j<height;j++){
+
+            average = round((image[j][i].rgbtBlue + image[j][i].rgbtGreen +image[j][i].rgbtRed)/3.00);
+            image[j][i].rgbtBlue = average;
+            image[j][i].rgbtGreen = average;
+            image[j][i].rgbtRed = average;
+
+        }
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
