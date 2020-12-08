e85dcabd8d760269f0b02abb374353bac88048a0
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-12-08 17:14:31 +0300

    create barebone, check argv, create buffers for 512 bytes, free mallocs

--- W4 Forensics/recover.c
+++ W4 Forensics/recover.c
@@ -13,4 +13,37 @@
 int main(int argc, char *argv[])
 {
  
+
+ // lets check the argcs
+
+ if(argc !=2){
+     fprintf(stderr,"Program takes only one parameter which is the forensic image.\n");
+     return EXIT_FAILURE;
+ }
+
+FILE *forensic= fopen("argv[1]", "r");
+if(forensic==NULL){
+    fprintf(stderr,"File %s could not be opened\n",argv[1]);
+}
+
+// lets define some generic values to use iteratively
+
+
+FILE *image;  // this is for the new file.
+int counter = 0; // number of founded items
+char name[8]; // 000.jpg + \0
+char *buffer = malloc(512); // This is the 512 byte buffer are to read and write the chunks.
+
+
+
+
+
+
+
+
+
+fclose(image);
+fclose(forensic);
+free(buffer);
+
 }
